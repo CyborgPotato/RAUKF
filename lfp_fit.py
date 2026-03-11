7eb4fc074c2d33469e0c80474e6b7bb3e5c33946
david <davidcrompton@duck.com>
2026-03-11 15:49:36 -0400

    Balanced EI Lif network

--- lfp_fit.py
+++ lfp_fit.py
@@ -16,54 +16,72 @@ import matplotlib.pyplot as plt
 class Exponential(bp.Projection):
   def __init__(self, pre, post, delay, prob, g_max, tau, E):
     super().__init__()
-    self.g_max_v = bm.Variable(1)
-    self.g_max_v.value = self.g_max_v.at[:].set(g_max)
-    self.pron = bp.dyn.FullProjAlignPost(
+    self.pron = bp.dyn.FullProjDelta(
       pre=pre,
       delay=delay,
       # Event-driven computation
-      comm=bp.dnn.EventCSRLinear(bp.conn.FixedProb(prob, pre=pre.num, post=post.num), self.g_max_v),
-      syn=bp.dyn.Expon(size=post.num, tau=tau),# Exponential synapse
-      out=bp.dyn.COBA(E=E), # COBA network
+      comm=bp.dnn.EventCSRLinear(bp.conn.FixedProb(prob, pre=pre.num, post=post.num), g_max),
+      # syn=bp.dyn.Expon(size=post.num, tau=tau),# Exponential synapse
+      # out=bp.dyn.COBA(E=E), # COBA network
       post=post
     )
 
 class EINet(bp.DynamicalSystem):
-  def __init__(self, noise=True, ne=100, ni=100):
+  def __init__(self, noise=True, N=1000, b=0.8, c=0.1):
     super().__init__()
     self.noise = noise
-    lif_pars = dict(V_rest=-60., V_th=-50., V_reset=-70., tau=20.,
-                    tau_ref=5.,V_initializer=bp.init.Normal(-55., 4.))
-    self.E = bp.dyn.LifRef(ne, **lif_pars)
-    self.I = bp.dyn.LifRef(ni, **lif_pars)
-    self.E2E = Exponential(self.E, self.E, 2., 0.02, 0.6*16*2, 2., 0.)
-    self.E2I = Exponential(self.E, self.I, 1., 0.02, 0.6*16, 2., 0.)
-    self.I2E = Exponential(self.I, self.E, 1., 0.02, 6.7*2, 5., -80.)
-    self.I2I = Exponential(self.I, self.I, 2., 0.02, 6.7*2, 5., -80.)
+    self.b = b
+    ne=int(b*N)
+    ni=int((1-b)*N)
+    self.J = 0.1
+    K = c*N
+    g1 = 100
+    self.Je = bm.Variable(bm.array([self.J*bm.sqrt(1000/K)]))
+    self.Ji = bm.Variable(bm.array([-(4+g1*bm.sqrt(c/K))*self.Je.value[0]]))
+    lif_pars = dict(
+      V_rest=0., V_th=20., V_reset=10., tau=20.,
+      tau_ref=.5, V_initializer=bp.init.Normal(0., 8.)
+    )
+    self.E = bp.dyn.LifRef(ne, **lif_pars, method='euler')
+    self.I = bp.dyn.LifRef(ni, **lif_pars, method='euler')
+    self.E2E = Exponential(self.E, self.E, .55, c, self.Je,  20., 0.)
+    self.E2I = Exponential(self.E, self.I, .55, c, self.Je,  20., 0.)
+    self.I2E = Exponential(self.I, self.E, .55, c, self.Ji,  20., -80.)
+    self.I2I = Exponential(self.I, self.I, .55, c, self.Ji,  20., -80.)
     self.input = bm.Variable(1,batch_axis=0)
-    self.input.value = self.input.at[:].set(100)
+    # self.input.value = self.input.at[:].set(500)
 
     self.lfp = bm.Variable(1,batch_axis=0)
     self.calc_lfp()
 
   def calc_lfp(self):
-    self.lfp.value = self.lfp.at[:].set(
-      -bm.sum(self.E2E.pron.syn.g) +\
-       bm.sum(self.I2E.pron.syn.g) +\
-       bm.sum(self.I2I.pron.syn.g) +\
-      -bm.sum(self.E2I.pron.syn.g)
-    )
+    self.lfp.value = self.lfp.at[:].set(self.E.V.mean()*self.b + self.I.V.mean()*(1-self.b))
+    # self.lfp.value = self.lfp.at[:].set(
+    #   -bm.sum(self.E2E.pron.syn.g) +\
+    #    bm.sum(self.I2E.pron.syn.g) +\
+    #    bm.sum(self.I2I.pron.syn.g) +\
+    #   -bm.sum(self.E2I.pron.syn.g)
+    # )
 
   def update(self):
+    t = bp.share['t']
+    self.input.value = self.input.at[:].set(
+      10*bm.sin(t*bm.pi*2/(1000/2))+10
+    )
+    # self.input.value = self.input.at[:].set(0)
     # if self.noise:
         # self.input.value +=  -(0.001*(self.input-500)) + bsr.normal(0,10,size=1)
+    self.E2E.pron.comm.weight.value = self.Je
+    self.E2I.pron.comm.weight.value = self.Je
+    self.I2E.pron.comm.weight.value = self.Ji
+    self.I2I.pron.comm.weight.value = self.Ji
     self.E2E()
     self.E2I()
     self.I2E()
     self.I2I()
     self.calc_lfp()
-    self.E(self.input)
-    self.I()
+    self.E(24+self.input)
+    self.I(24+self.input)
     return self.E.spike.value, self.I.spike.value
 
 import main as main_
@@ -89,7 +107,7 @@ def run_with_Q_R(params, inp=0, obs=0):
     obs
   )
   net_kf.t_stab = 100
-  net_kf.x.value = net_kf.x.at[-4:].set(net_kf.x.value[-4:]*1.5)
+  net_kf.x.value = net_kf.x.at[-4:].set(net_kf.x.value[-4:]*1)
   net_kf.Q.value = np.diag(np.array([
       lfp_Q,
       # inp_Q,
@@ -107,7 +125,7 @@ def run_with_Q_R(params, inp=0, obs=0):
       w_Q,
   ],dtype=np.float32))
 
-  net_kf.R.value = net_kf.R.at[:].set(250**2)
+  net_kf.R.value = net_kf.R.at[:].set(25)
 
   kf_run = bp.DSRunner(net_kf, monitors=['net.lfp','net.input'],progress_bar=False)
 
@@ -133,6 +151,10 @@ if __name__=='__main__':
   input = runner.mon['input']
 
   obs = observation#+np.random.normal(0,50,size=observation.shape)
+  # plt.plot(obs)
+  # plt.twinx()
+  # plt.plot(input,color='r')
+  # plt.show()
   # plt.scatter(*np.nonzero(runner.mon['E.spike']),marker='|')
   # plt.figure()
   # plt.scatter(*np.nonzero(runner.mon['I.spike']),marker='|')
@@ -170,14 +192,15 @@ if __name__=='__main__':
     ],
     [ # What states to estimate
       # r'.*input$',
-      r'.*weight$'
+      r'.*Je$',
+      r'.*Ji$',
     ],
     [ # What our measurement/observation is
       r'.*lfp$',
     ],
     obs
   )
-  net_kf.t_stab = 100
+  net_kf.t_stab = 300
 
   # net_kf.R.value = net_kf.R.at[:].set(1e-6)
   # net_kf.robust = False
@@ -189,48 +212,42 @@ if __name__=='__main__':
 
   # net_kf.x.value = net_kf.x.at[1].set(11)
 
-  lfp_Q = 1e0
+  lfp_Q = 1e-15
   # inp_Q = 1e16
-  w_Q = 1e-3
+  w_Q = 1e-0
   # net_kf.x.value = net_kf.x.at[1].set(net_kf.x.value[1]*1)
-  net_kf.x.value = net_kf.x.at[-4:].set(net_kf.x.value[-4:]*1.25)
+  net_kf.x.value = net_kf.x.at[-2:].set(net_kf.x.value[-2:]*8)
   net_kf.Q.value = np.diag(np.array([
-      lfp_Q,
-      # inp_Q,
-      w_Q,
-      w_Q,
-      w_Q,
-      w_Q,
+    lfp_Q,
+    # inp_Q,
+    w_Q,
+    w_Q,
   ],dtype=np.float32))
   net_kf.P.value = np.diag(np.array([
-      lfp_Q,
-      # inp_Q,
-      w_Q,
-      w_Q,
-      w_Q,
-      w_Q,
+    lfp_Q,
+    # inp_Q,
+    w_Q,
+    w_Q,
   ],dtype=np.float32))
 
-  net_kf.R.value = net_kf.R.at[:].set(np.power(10,np.ceil(np.log10(np.std(obs)**2))))
+  net_kf.R.value = net_kf.R.at[:].set(1e-8)
 
   net_kf.resample = False
-  net_kf.adjust_every = 2.5
+  net_kf.adjust_every = 100
   
   net_kf.robust_after = 0
   net_kf.robust = False
-  net_kf.lambda0 = 0
-  net_kf.delta0 = 0
+  net_kf.lambda0 = 0.2
+  net_kf.delta0 = 0.2
   net_kf.a = 10
-  net_kf.b = 5
-  net_kf.threshold = 0.1
+  net_kf.b = 10
+  net_kf.threshold = 0.45
 
   kf_run = bp.DSRunner(net_kf, monitors=[
     'net.lfp',
-    'net.input',
-    'net.E2E.pron.comm.weight',
-    'net.E2I.pron.comm.weight',
-    'net.I2E.pron.comm.weight',
-    'net.I2I.pron.comm.weight',
+    'net.input', 
+    'net.Je',
+    'net.Ji',
     'phi',
   ])
 
@@ -243,15 +260,11 @@ if __name__=='__main__':
   plt.plot(obs,color='g')
   plt.plot(kf_lfp,linestyle=':',color='r')
   plt.figure()
-  plt.plot(kf_run.mon['net.E2E.pron.comm.weight'],color='r')
-  plt.hlines(net.E2E.pron.comm.weight.value,0,obs.size,color='r',linestyle='--')
-  plt.plot(kf_run.mon['net.E2I.pron.comm.weight'],color='r')
+  plt.plot(kf_run.mon['net.Je'],color='r')
   plt.hlines(net.E2I.pron.comm.weight.value,0,obs.size,color='r',linestyle='--')
-  plt.plot(kf_run.mon['net.I2E.pron.comm.weight'],color='g')
-  plt.hlines(net.I2E.pron.comm.weight.value,0,obs.size,color='g',linestyle='--')
-  plt.plot(kf_run.mon['net.I2I.pron.comm.weight'],color='g')
+  plt.plot(kf_run.mon['net.Ji'],color='g')
   plt.hlines(net.I2I.pron.comm.weight.value,0,obs.size,color='g',linestyle='--')
-  plt.figure()
-  plt.plot(input,color='k')
-  plt.plot(kf_input,linestyle=':',color='r',linewidth=5)
+  # plt.figure()
+  # plt.plot(input,color='k')
+  # plt.plot(kf_input,linestyle=':',color='r',linewidth=5)
   plt.show()
